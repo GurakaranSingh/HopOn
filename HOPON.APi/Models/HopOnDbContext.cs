c5d57ed081d656b8ec25e474d0e1c0395de36a45
karan virdi <karanvirdi16@live.com>
2021-06-21 11:27:33 +0530

    Work related to API

---
+++ HOPON.APi/Models/HopOnDbContext.cs
@@ -0,0 +1,34 @@
+﻿using HopOn.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HOPON.APi.Models
+{
+    public class HopOnDbContext :DbContext
+    {
+        public HopOnDbContext(DbContextOptions<HopOnDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<FileListModel> fileLists { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Code to seed data
+            modelBuilder.Entity<FileListModel>().HasData(
+                 new FileListModel { ID = 1, Name = "IT", PhotoPath = "Images/file4.png" });
+            modelBuilder.Entity<FileListModel>().HasData(
+                new FileListModel { ID = 1, Name = "TPSReport.Xlsx", PhotoPath = "Images/file1.png" });
+            modelBuilder.Entity<FileListModel>().HasData(
+                new FileListModel { ID = 1, Name = "CannedReport.ppt", PhotoPath = "Images/file2.png" });
+            modelBuilder.Entity<FileListModel>().HasData(
+                new FileListModel { ID = 1, Name = "NewImage.jpeg", PhotoPath = "Images/file3.png" });
+            modelBuilder.Entity<FileListModel>().HasData(
+                new FileListModel { ID = 1, Name = "TextBook.png", PhotoPath = "Images/file4.png" });
+
+
+        }
+    }
+}
